66a45c37b75e44b220bbd7fdd3985d370805ffa4
vigneshwara palanisamy <vigneshwara.palanisamy@nokia.com>
2023-10-18 11:12:29 +0530

    Created box functionality

---
+++ src/boxes.js
@@ -0,0 +1,363 @@
+export default [
+    {
+        id: 1,
+        on: true
+    },   
+    {
+        id: 2,
+        on: false
+    },   
+    {
+        id: 3,
+        on: true
+    },   
+    {
+        id: 4,
+        on: true
+    },   
+    {
+        id: 5,
+        on: false
+    },   
+    {
+        id: 6,
+        on: false
+    },
+    {
+        id: 7,
+        on: true
+    },   
+    {
+        id: 8,
+        on: false
+    },   
+    {
+        id: 9,
+        on: true
+    },   
+    {
+        id: 10,
+        on: true
+    },
+    {
+        id: 11,
+        on: true
+    },   
+    {
+        id: 12,
+        on: false
+    },   
+    {
+        id: 13,
+        on: true
+    },   
+    {
+        id: 14,
+        on: true
+    },   
+    {
+        id: 15,
+        on: false
+    },   
+    {
+        id: 16,
+        on: false
+    },
+    {
+        id: 17,
+        on: true
+    },   
+    {
+        id: 18,
+        on: false
+    },   
+    {
+        id: 19,
+        on: true
+    },   
+    {
+        id: 20,
+        on: true
+    },
+    {
+        id: 21,
+        on: true
+    },   
+    {
+        id: 22,
+        on: false
+    },   
+    {
+        id: 23,
+        on: true
+    },   
+    {
+        id: 24,
+        on: true
+    },   
+    {
+        id: 25,
+        on: false
+    },   
+    {
+        id: 26,
+        on: false
+    },
+    {
+        id: 27,
+        on: true
+    },   
+    {
+        id: 28,
+        on: false
+    },   
+    {
+        id: 29,
+        on: true
+    },   
+    {
+        id: 30,
+        on: false
+    },
+    {
+        id: 31,
+        on: true
+    },   
+    {
+        id: 32,
+        on: false
+    },   
+    {
+        id: 33,
+        on: true
+    },   
+    {
+        id: 34,
+        on: true
+    },   
+    {
+        id: 35,
+        on: false
+    },   
+    {
+        id: 36,
+        on: false
+    },
+    {
+        id: 37,
+        on: false
+    },   
+    {
+        id: 38,
+        on: false
+    },   
+    {
+        id: 39,
+        on: true
+    },   
+    {
+        id: 40,
+        on: true
+    },
+    {
+        id: 41,
+        on: true
+    },   
+    {
+        id: 42,
+        on: false
+    },   
+    {
+        id: 43,
+        on: true
+    },   
+    {
+        id: 44,
+        on: false
+    },   
+    {
+        id: 45,
+        on: false
+    },   
+    {
+        id: 46,
+        on: false
+    },
+    {
+        id: 47,
+        on: false
+    },   
+    {
+        id: 48,
+        on: false
+    },   
+    {
+        id: 49,
+        on: true
+    },   
+    {
+        id: 50,
+        on: true
+    },
+    {
+        id: 51,
+        on: true
+    },   
+    {
+        id: 52,
+        on: false
+    },   
+    {
+        id: 53,
+        on: false
+    },   
+    {
+        id: 54,
+        on: true
+    },   
+    {
+        id: 55,
+        on: false
+    },   
+    {
+        id: 56,
+        on: true
+    },
+    {
+        id: 57,
+        on: true
+    },   
+    {
+        id: 58,
+        on: false
+    },   
+    {
+        id: 59,
+        on: true
+    },   
+    {
+        id: 60,
+        on: true
+    },
+    {
+        id: 61,
+        on: false
+    },   
+    {
+        id: 62,
+        on: true
+    },   
+    {
+        id: 63,
+        on: false
+    },   
+    {
+        id: 64,
+        on: true
+    },   
+    {
+        id: 65,
+        on: false
+    },   
+    {
+        id: 66,
+        on: false
+    },
+    {
+        id: 67,
+        on: false
+    },   
+    {
+        id: 68,
+        on: false
+    },   
+    {
+        id: 69,
+        on: false
+    },   
+    {
+        id: 70,
+        on: true
+    },
+    {
+        id: 71,
+        on: true
+    },   
+    {
+        id: 72,
+        on: false
+    },   
+    {
+        id: 73,
+        on: true
+    },   
+    {
+        id: 74,
+        on: true
+    },   
+    {
+        id: 75,
+        on: true
+    },   
+    {
+        id: 76,
+        on: false
+    },
+    {
+        id: 77,
+        on: true
+    },   
+    {
+        id: 78,
+        on: true
+    },   
+    {
+        id: 79,
+        on: true
+    },   
+    {
+        id: 80,
+        on: true
+    },
+    {
+        id: 81,
+        on: true
+    },   
+    {
+        id: 82,
+        on: false
+    },   
+    {
+        id: 83,
+        on: true
+    },   
+    {
+        id: 84,
+        on: true
+    },   
+    {
+        id: 85,
+        on: false
+    },   
+    {
+        id: 86,
+        on: false
+    },
+    {
+        id: 87,
+        on: false
+    },   
+    {
+        id: 88,
+        on: false
+    },   
+    {
+        id: 89,
+        on: true
+    },   
+    {
+        id: 90,
+        on: true
+    },
+
+]
